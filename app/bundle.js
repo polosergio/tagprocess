02a4e5fb85c3d7d462e13a82cdb709d457c7e05c
Sergio Polo <sergio@toppatch.com>
2014-02-21 01:47:04 -0500

    Latest compiled bundle

--- app/bundle.js
+++ app/bundle.js
@@ -56,7 +56,7 @@ module.exports = (function () {
 
 		_doSignIn: function (options) {
 			var _this = this,
-				defaults = { url: '/login', type: 'POST', contentType: 'application/json', processData: false },
+				defaults = { url: '/tagproc/api/login', type: 'POST', contentType: 'application/json', processData: false },
 				settings = _.extend(defaults, options);
 			return $
 				.ajax(settings)
@@ -105,7 +105,7 @@ module.exports = (function () {
                 var _this = this;
                 return $
                     .ajax({
-                        url: '/logout',
+                        url: '/tagproc/api/logout',
                         type: 'get'
                     })
                     .done(
