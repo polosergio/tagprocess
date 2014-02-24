aba6b61d9ea6f4269ad2ed6f6905040671dbddfb
sergiopolo <sergio@toppatch.com>
2014-02-24 01:51:56 -0500

    Fixes bug where pressing enter on search submits a form and fixes ability to search by account and case

--- app/js/modules/client.js
+++ app/js/modules/client.js
@@ -16,12 +16,14 @@ module.exports = (function () {
 			url: function () {
 				return this.baseUrl + '?' + $.param(this.params);
 			},
-			parse: function (response) {
+			parse: function (response, xhr) {
+                this.status = xhr.xhr.status;
 				return response;
 			}
 		}),
 		View: Backbone.View.extend({
 			initialize: function () {
+                var that = this;
 				this.template = ClientTemplate;
 				this.collection = new exports.Collection();
 				delete this.collection.params.q;
@@ -31,13 +33,15 @@ module.exports = (function () {
 			},
 			events: {
 				'click #searchby a': 'setSearchBy',
-				'keyup #search': 'search'
+				'keyup #search': 'debouncedSearch',
+                'submit form': 'search'
 			},
 			render: function () {
 				var data = this.collection.toJSON(),
 					payload = {
 					items: _.isEmpty(data) ? null : data,
-					form: this.collection.params
+					form: this.collection.params,
+                    no_access: _.isUndefined(this.collection.status) || this.collection.status === 401
 				};
 				this.$el.empty().append(this.template(payload));
 				return this;
@@ -49,11 +53,15 @@ module.exports = (function () {
 				$button.html($target.html());
 				this.collection.params.searchby = $target.data('value');
 			},
-			search: _.debounce(function (event) {
+            debouncedSearch: _.debounce(function (event) {
+                this.search(event);
+            }, 1000),
+			search: function (event) {
+                event.preventDefault();
 				this.collection.params.q = this.$('#search').val();
 				this.collection.params.searchby = this.collection.params.searchby ? this.collection.params.searchby : 'jobnumber';
 				this.collection.fetch();
-			}, 1000)
+			}
 		})
 	});
 	return exports;
